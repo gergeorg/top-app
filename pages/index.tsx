0afd39172c263353571e39bfe11302163ff09bd4
gergeorg <georgey.gerasimov@gmail.com>
2023-03-28 11:48:11 +0300

    add rating, layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
-import { Button, Htag, P, Tag } from '../components'
+import { useState } from 'react'
+import { Button, Htag, P, Tag, Rating } from '../components'
+import { withLayout } from '../layout/Layout'
 
-export default function Home(): JSX.Element {
+function Home(): JSX.Element {
+	const [rating, setRating] = useState<number>(4)
+
 	return (
 		<>
 			<Htag tag='h1'>Курсы по Photoshop</Htag>
+			<Rating rating={rating} isEditable setRating={setRating} />
 		</>
 	)
 }
+
+export default withLayout(Home)
