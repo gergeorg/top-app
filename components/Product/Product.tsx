f79b91e7cff68402eb8a496afbda7d8036b1fe20
Georgey Gerasimov <georgey.gerasimov@gmail.com>
2023-04-02 16:17:10 +0300

    add product adaptive

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import Image from 'next/image'
+
 import { ProductProps } from './Product.props'
-import cn from 'classnames'
+
 import styles from './Product.module.css'
+import cn from 'classnames'
+
 import { Card } from '../Card/Card'
 import { Rating } from '../Rating/Rating'
 import { Tag } from '../Tag/Tag'
@@ -10,11 +14,11 @@ import { priceRu, declOfNum } from '../../helpers/helpers'
 import { Divider } from '../Divider/Divider'
 import { P } from '../P/P'
 
-export const Product = ({ product, className, ...props }: ProductProps): JSX.Element => {
+export const Product = ({ product }: ProductProps): JSX.Element => {
 	return (
 		<Card className={styles.product}>
 			<div className={styles.logo}>
-				<img src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title}></img>
+				<Image src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title} width={70} height={70} />
 			</div>
 			<div className={styles.title}>{product.title}</div>
 			<div className={styles.price}>
@@ -80,7 +84,7 @@ export const Product = ({ product, className, ...props }: ProductProps): JSX.Ele
 				)}
 			</div>
 
-			<Divider className={styles.hr} />
+			<Divider className={cn(styles.hr, styles.hr2)} />
 
 			<div className={styles.actions}>
 				<Button className={styles.readMoreBtn} appearance='primary'>
