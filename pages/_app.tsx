573f784da23d69fd2a1fb87decb905cb1a36f3c8
gergeorg <georgey.gerasimov@gmail.com>
2023-04-12 01:33:07 +0300

    husky

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import Router from 'next/router'
+import React from 'react'
+import ym from 'react-yandex-metrika'
+import { YMInitializer } from 'react-yandex-metrika'
 
-export default function App({ Component, pageProps }: AppProps) {
+Router.events.on('routeChangeComplete', (url: string) => {
+	if (typeof window !== 'undefined') {
+		ym('hit', url)
+	}
+})
+
+export default function App({ Component, pageProps, router }: AppProps) {
 	return (
 		<>
 			<Component {...pageProps} />
@@ -12,11 +22,15 @@ export default function App({ Component, pageProps }: AppProps) {
 				<link rel='icon' href='/favicon.ico' />
 				<link rel='preconnect' href='https://fonts.googleapis.com' />
 				<link rel='preconnect' href='https://fonts.gstatic.com' />
+				<link rel='preconnect' href='c' />
 				<link
-					href='https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap'
+					href='https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap'
 					rel='stylesheet'
 				/>
+				<meta property='og:url' content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
+				<meta property='og:locale' content='ru_RU' />
 			</Head>
+			<YMInitializer accounts={[]} options={{ webvisor: true, defer: true }} version='2' />
 		</>
 	)
 }
